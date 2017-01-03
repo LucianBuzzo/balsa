c7d3544ae49d094ac8cc218f2bc1681709483a2d
Lucian Buzzo <lucian@gravitywell.co.uk>
2017-01-03 22:43:07 +0000

    The "isObject" method will now only return true for object literals.

--- balsa.js
+++ balsa.js
@@ -43,10 +43,7 @@
   };
 
   var isObject = function isObject(value) {
-    // Avoid a V8 JIT bug in Chrome 19-20.
-    // See https://code.google.com/p/v8/issues/detail?id=2291 for more details.
-    var type = typeof value;
-    return type === 'function' || value && type === 'object' || false;
+    return Object.prototype.toString.call(value) === '[object Object]';
   };
 
   var clone = function clone(value) {
